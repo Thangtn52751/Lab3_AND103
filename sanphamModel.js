121ed355b6794c004ec8ab3180c83f9230a5013d
Thangtn52751 <thangtnph52751@gmail.com>
2024-11-11 01:55:50 +0700

    Lab3

---
+++ sanphamModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const SanphamSchema = mongoose.Schema({
+
+    ten: {
+        type: String,
+        required: true
+    },
+
+    gia: {
+        type: Number,
+        required: true
+    },
+
+    soluong: {
+        type: Number,
+        required: true
+    },
+
+    tonkho: {
+        type: Boolean
+    },
+
+});
+
+const SanphamModel = mongoose.model('user', SanphamSchema);
+
+module.exports = SanphamModel;
